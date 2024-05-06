7e0e8068e8d059559ef02a4cd4d3ebc76af2056c
yazidalg <yazid.al2418@gmail.com>
2024-05-06 23:04:06 +0700

    add report models

---
+++ LaporanKtmAPI/Models/Laporan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LaporanKtmAPI.Model
+{
+    public class Laporan
+    {
+        public string Name { get; set; }
+        public string EmailSSO { get; set; }
+        public string Nim { get; set; }
+        public State Status { get; set; }
+        public Laporan(string name, string emailSSO, string nim, State status)
+        {
+            Name = name;
+            EmailSSO = emailSSO;
+            Nim = nim;
+            Status = status;
+        }
+    }
+}
