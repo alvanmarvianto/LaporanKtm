992221d86334bf986aa09b8e770c84379472b6a2
fharlup <35958282+fharlup@users.noreply.github.com>
2024-05-06 22:55:52 +0700

    statebase

--- LaporanKtm/Enum.cs
+++ LaporanKtm/Enum.cs
@@ -1,68 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
 
-// Mendefinisikan state dalam automata
 public enum State
 {
     Start,
     MembuatLaporan,
     MengeditLaporan,
-    MencariLaporan,
-    MembacaLaporan
-}
-public enum Trigger { proses, cancel };
-
-// Mendefinisikan transisi dalam automata
-class Transition
-{
-    public State FromState { get; set; }
-    public State ToState { get; set; }
-    public State FinalState { get; set; }
-    public string Input { get; set; }
+    Ketemu
 }
 
-// Mensimulasikan alur interaksi dalam aplikasi
-public class AutomataSimulator
+public enum Trigger { proses, cancel, cari, edit };
+
+class StateTodo
 {
-    private State currentState;
-
-    public AutomataSimulator()
+    public class Transition
     {
-        currentState = State.Start;
-    }
+        public State StateAwal;
+        public State StateAkhir;
+        public Trigger Trigger;
 
-    public void ProcessInput(string input)
-    {
-        // Mencari transisi yang sesuai dengan state dan input saat ini
-        Transition transition = FindTransition(currentState, input);
-
-        if (transition != null)
+        public Transition(State stateAwal, State stateAkhir, Trigger trigger)
         {
-            // Memperbarui state
-            currentState = transition.ToState;
-
-            // Melakukan tindakan berdasarkan state baru
-            switch (currentState)
-            {
-                case State.MembuatLaporan:
-                    // Tampilkan formulir untuk membuat laporan
-                    break;
-                case State.MengeditLaporan:
-                    // Tampilkan formulir untuk mengedit laporan
-                    break;
-                case State.MencariLaporan:
-                    // Tampilkan formulir untuk mencari laporan
-                    break;
-                case State.MembacaLaporan:
-                    // Tampilkan laporan yang dipilih
-                    break;
-            }
+            this.StateAwal = stateAwal;
+            this.StateAkhir = stateAkhir;
+            this.Trigger = trigger;
         }
     }
 
-    private Transition FindTransition(State currentState, string input)
+    Transition[] transisi =
     {
-        // Implementasikan logika untuk mencari transisi yang sesuai
-        // ...
+        new Transition(State.Start, State.MembuatLaporan, Trigger.proses),
+        new Transition(State.MembuatLaporan, State.Ketemu, Trigger.cari),
+        new Transition(State.MembuatLaporan, State.Start, Trigger.cancel),
+        new Transition(State.MembuatLaporan, State.MengeditLaporan, Trigger.edit),
+        new Transition(State.MengeditLaporan, State.MembuatLaporan, Trigger.proses)
+    };
+
+    public State currentState = State.Start;
+    public Dictionary<string, State> tasks = new Dictionary<string, State>();
+
+    public State GetNextState(State stateAwal, Trigger trigger)
+    {
+        foreach (Transition perubahan in transisi)
+        {
+            if (stateAwal == perubahan.StateAwal && trigger == perubahan.Trigger)
+            {
+                return perubahan.StateAkhir;
+            }
+        }
+        return stateAwal;
     }
+
+    public void ActivateTrigger(Trigger trigger)
+    {
+        currentState = GetNextState(currentState, trigger);
+        Console.WriteLine("State Anda adalah: " + currentState);
+    }
+
+    public void AddTask(string task, State taskState)
+    {
+        tasks.Add(task, taskState);
+        Console.WriteLine("Tambah task: " + task + " (State: " + taskState + ")");
+    }
+
+    public void DisplayTasks()
+    {
+        Console.WriteLine("Daftar task:");
+        foreach (var task in tasks)
+        {
+            Console.WriteLine("- " + task.Key + " (State: " + task.Value + ")");
+        }
+    }
+
+    public void ChangeTaskState(string task, State newState)
+    {
+        if (tasks.ContainsKey(task))
+        {
+            tasks[task] = newState;
+            Console.WriteLine("ktm  task '" + task + "' berhasil diubah menjadi: " + newState);
+        }
+        else
+        {
+            Console.WriteLine("ktm '" + task + "' tidak ditemukan.");
+        }
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Daftar trigger yang tersedia:");
+        foreach (Trigger trigger in Enum.GetValues(typeof(Trigger)))
+        {
+            Console.WriteLine("- " + trigger);
+        }
+
+        Console.WriteLine();
+        Console.Write("Masukkan jumlah task yang ingin ditambahkan: ");
+        int jumlahTask = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < jumlahTask; i++)
+        {
+            Console.Write("Masukkan nama task ke-" + (i + 1) + ": ");
+            string namaTask = Console.ReadLine();
+            AddTask(namaTask, State.Start);
+        }
+
+        DisplayTasks();
+
+        Console.Write("Masukkan nama task yang ingin diubah statusnya: ");
+        string taskYangDiubah = Console.ReadLine();
+
+        Console.WriteLine("Daftar trigger yang tersedia:");
+        foreach (Trigger trigger in Enum.GetValues(typeof(Trigger)))
+        {
+            Console.WriteLine("- " + trigger);
+        }
+
+        Console.WriteLine();
+        Console.Write("Pilih trigger untuk task '" + taskYangDiubah + "': ");
+        string triggerInput = Console.ReadLine();
+
+        if (Enum.TryParse(triggerInput, out Trigger selectedTrigger))
+        {
+            ActivateTrigger(selectedTrigger);
+            DisplayTasks();
+        }
+        else
+        {
+            Console.WriteLine("Trigger tidak valid.");
+        }
+    }
+
 }
