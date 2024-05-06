d083f9fdb40b1abbc5543fd3545629e590a423a4
yazidalg <yazid.al2418@gmail.com>
2024-05-06 23:08:08 +0700

    get action

---
+++ LaporanKtmAPI/Controllers/LaporanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LaporanKtmAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaporanKtmAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LaporanController : ControllerBase
+    {
+        private static List<Laporan> reportList = new List<Laporan>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Laporan>> Get()
+        {
+            try
+            {
+                return CollectionHelper.GetAll<Laporan>(reportList);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
+
