b6d68a7850f5bc3724b6515fc609cd0358393ea9
yazidalg <yazid.al2418@gmail.com>
2024-05-07 00:17:00 +0700

    add date in model

--- LaporanKtmAPI/Models/Laporan.cs
+++ LaporanKtmAPI/Models/Laporan.cs
@@ -8,12 +8,14 @@ namespace LaporanKtmAPI.Model
         public string EmailSSO { get; set; }
         public string Nim { get; set; }
         public State Status { get; set; }
-        public Laporan(string name, string emailSSO, string nim, State status)
+        public DateOnly Date { get; set; }
+        public Laporan(string name, string emailSSO, string nim, State status, DateOnly date)
         {
             Name = name;
             EmailSSO = emailSSO;
             Nim = nim;
             Status = status;
+            Date = date;
         }
     }
 }
