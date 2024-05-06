8a688f08780469d12f95b167e6e65c175b4bec26
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-06 23:26:51 +0700

    Menambahkan Table Konsep Driven

---
+++ LaporanKtm/TableDrivenState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaporanKtm
+{
+    public class TableDrivenState
+    {
+        public enum state
+        {
+            Start,
+            MembuatLaporan,
+            MengeditLaporan,
+            MencariLaporan,
+            MembacaLaporan
+        }
+
+        public static readonly Dictionary<string, state> STATE = new Dictionary<string, state>
+        {
+            {"Start", state.Start},
+            {"MembuatLaporan", state.MembuatLaporan},
+            {"MengeditLaporan",state.MengeditLaporan},
+            {"MencariLaporan", state.MencariLaporan},
+            {"MembacaLaporan", state.MembacaLaporan},
+        };
+        public static state getstate(string State)
+        {
+            return STATE[State];
+        }
+    }
+}
+
