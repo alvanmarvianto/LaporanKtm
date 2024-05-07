500561cf5e22b829b5a0781023aebbbbf8be3344
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-07 14:33:50 +0700

    Add files via upload
    "testing unit"

---
+++ JeremyTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaporanKtm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LaporanKtmTest
+{
+    [TestClass]
+    public class TableDrivenBulanTests
+    { 
+       [TestMethod]
+        public void GetBulan_ReturnsCorrectValue()
+        {
+        // Arrange
+        var expectedBulan = TableDrivenBulan.bulan.Februari;
+
+        // Act
+        var actualBulan = TableDrivenBulan.getbulan("Februari");
+
+        // Assert
+        Assert.AreEqual(expectedBulan, actualBulan);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void GetBulan_ThrowsExceptionForInvalidInput()
+        {
+         // Act & Assert
+        TableDrivenBulan.getbulan("InvalidBulanName");
+        }
+    }
+}
